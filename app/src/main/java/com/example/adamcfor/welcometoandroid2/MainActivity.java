e8c0232594792d3a98b83b545e24b85b5dfcce89
Adam Or <adam.cf.or@gtomato.com>
2019-01-04 19:16:18 +0800

    ADD login page

--- app/src/main/java/com/example/adamcfor/welcometoandroid2/MainActivity.java
+++ app/src/main/java/com/example/adamcfor/welcometoandroid2/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.adamcfor.welcometoandroid2;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+//    Constants
+    public final static String USER_NAME = "USER_NAME";
+    public static final String USER_PASSWORD = "USER_PASSWORD";
+    public static final String USER_DATA = "USER_DATA";
+
+//    Variables
+    private EditText etEmail;
+    private EditText etPassword;
+    private SharedPreferences sharedPreferences;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etPassword = (EditText) findViewById(R.id.etPassword);
+
+        sharedPreferences = getSharedPreferences(USER_DATA, Context.MODE_PRIVATE);
+//        if (sharedPreferences.getString(USER_NAME, "") == null) {
+//            openRegisterDialog();
+//        }
+    }
+
+    private void openRegisterDialog() {
+        RegisterDialog registerDialog = new RegisterDialog();
+        registerDialog.show(getSupportFragmentManager(), "registerDialog");
+
+    }
+
+    public void userLogin(View view) {
+    }
+
+
+    public void userRegister(View view) {
+        openRegisterDialog();
+    }
+
+    public void userRegisterCheck(View view) {
+
     }
 }
---
+++ app/src/main/java/com/example/adamcfor/welcometoandroid2/RegisterDialog.java
@@ -0,0 +1,102 @@
+package com.example.adamcfor.welcometoandroid2;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.text.Editable;
+import android.text.InputType;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.util.Patterns;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class RegisterDialog extends AppCompatDialogFragment {
+    private EditText etRegisterEmail;
+    private EditText etRegisterPassword;
+    private EditText etRegisterPasswordReenter;
+    private Button btnUserRegisterCheck;
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        final View view = inflater.inflate(R.layout.layout_dialog, null);
+
+        builder.setView(view).setTitle("Register");
+//                .setPositiveButton("register", new DialogInterface.OnClickListener() {
+//                    @Override
+//                    public void onClick(DialogInterface dialog, int which) {
+//                        saveInfo(view);
+//                    }
+//                });
+
+        etRegisterEmail = (EditText) view.findViewById(R.id.etRegisterEmail);
+        etRegisterPassword = (EditText) view.findViewById(R.id.etRegisterPassword);
+        etRegisterPasswordReenter = (EditText) view.findViewById(R.id.etRegisterPasswordReenter);
+
+        etRegisterEmail.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {}
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String email = etRegisterEmail.getText().toString();
+                if (!TextUtils.isEmpty(email) && (android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches())) {
+                    btnUserRegisterCheck.setEnabled(true);
+                } else {
+                    btnUserRegisterCheck.setEnabled(false);
+                }
+            }
+        });
+
+        btnUserRegisterCheck = view.findViewById(R.id.btUserRegisterCheck);
+        btnUserRegisterCheck.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                saveInfo(view);
+            }
+        });
+
+        etRegisterEmail.setInputType(InputType.TYPE_CLASS_TEXT|InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        etRegisterPassword.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+        etRegisterPasswordReenter.setInputType(InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD);
+
+        return builder.create();
+    }
+
+    private void saveInfo(View view) {
+        String pw1 = etRegisterPassword.getText().toString();
+        String pw2 = etRegisterPasswordReenter.getText().toString();
+        if (pw1.equals(pw2)) {
+            SharedPreferences sharedPreferences = getContext().getSharedPreferences(MainActivity.USER_DATA, Context.MODE_PRIVATE);
+            SharedPreferences.Editor editor = sharedPreferences.edit();
+            editor.putString(MainActivity.USER_NAME, etRegisterEmail.getText().toString());
+            editor.putString(MainActivity.USER_PASSWORD, etRegisterPassword.getText().toString());
+            editor.apply();
+            Toast.makeText(this.getContext(), "Registered", Toast.LENGTH_LONG).show();
+        } else {
+
+            etRegisterPassword.setHint("Two passwords are not match");
+            etRegisterPasswordReenter.setHint("Two passwords are not match");
+
+            etRegisterPassword.setText("");
+            etRegisterPasswordReenter.setText("");
+
+        }
+
+    }
+
+
+}
